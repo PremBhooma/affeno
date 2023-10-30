3a1d2e63189ab33766cb9dc5f556e64d1f98ab8e
PremBhooma <bhoomasagar1213@gmail.com>
2023-10-30 16:25:43 +0530

    trend

---
+++ src/Components/Trend.jsx
@@ -0,0 +1,132 @@
+import React from "react";
+import "./Trend.css";
+
+const Trend = () => {
+  return (
+    <>
+      <div id="carouselExample" className="carousel slide">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <div className="cards-wrapper">
+              <div className="card">
+                <img
+                  src="https://s3-alpha-sig.figma.com/img/0a93/0520/6a13d4463239359d29c9d81e2b24b42c?Expires=1699833600&Signature=lt6rd9IIdk22CSVOIf3kvoUtGEBgidJV9rPL1HbbCqzHMm~99QrqCM0vjk5NOUz-v8knsiZnolNCsV~YwxFO~5lNJb7BgChadNzzTgwUEdIECVuWOzYoy1Kbbq~Fls9pIgM3LNRaTVGIb5yo9QEJ9jE76iZ1GStbCSOvZOS5n15PPQmXwgHmyV6b7WAMfONvz0Jb9uqd6R5kfmEelC9-uy5dxrC1VxFHm7pM4wiLRvpTg2-o8VMuz5S1lyVjn54GKlD7PLzSTpt2YCMrKJ2~8vAnwZ4Sml~UmW-dvficAriElr5yyJPdfA7yF9DNl9S8hvq1hyaQojdJDFLeO-8m8g__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div class="card-body">
+                  <h5 class="card-title">Sun Glasses</h5>
+                  <p class="card-text">Extra 30% off</p>
+                </div>
+              </div>
+              <div className="card d-none d-md-block">
+                <img
+                  src="https://s3-alpha-sig.figma.com/img/50e5/9501/6317994781a1c2eda0bc69a932bd6f3f?Expires=1699833600&Signature=ECj5653myZGPrgm2xyd1JLQLAmtgYmrLAWJDXJxBT5Hsce2Z9HoAj8kYNYCAu4LygDndZOLwTG2OA~QV283kSU5aEhlEknU3D-AOLdtzwY7UMPjDphii5~YgnU8Gc9LOZ2Nj2AsXeaarv9WhrCt2A8dQEpq9OFqHnx29cVPVq0AI2bs1UwohcD5xy4yXiozzSSF-iCYobz45M9lybw2MmGAYWpLjI50EzakF5BwpK3cnkdXdCw7y~qiuxd~FJnkADdh7S4j~Fx94R2W05EFbsM6NOSEV4~0hay0i9eXO~YhMuz6h2ck~kcVsvpRWH6Rs~0cOTQGq9XTqiyMdamLOaQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div class="card-body">
+                  <h5 class="card-title">Men Shoes</h5>
+                  <p class="card-text">Extra 30% off</p>
+                </div>
+              </div>
+              <div className="card d-none d-md-block">
+                <img
+                  src="https://s3-alpha-sig.figma.com/img/8923/5459/e3b1cc3f6a9e3ad8bafb665e1eb54ab7?Expires=1699833600&Signature=eMEowDMnvUQR1I38DWphwfTuH9DWmV9jKqWPHtiBeu7BJJer2RLW7mYSIXzgR~pnRQuaufhlTIgdc6KlYEs2UL5RyPjbW2-SryC6N43V2xveXSCCRsjJRBeEFDr5VGTPBkha7YjmjDeVn4MLu3tjvU~UUsCOEdgjqH62j~EvYubqrLVuMYCraJ7Lz98QsNaOwLQEoMarl4bzcuws642hy55pFZs2AcYSWXDVMCHLv8ujrqPnusdv7XvoMc8mOwYdmsR1mwQS3cnbDqSiVSshTSWtypPph2wqExDIZ07j14Puf~59OO9caXjf1bF-pZL5tkQ7zVqut11Ub~ERAEI0gA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div class="card-body">
+                  <h5 class="card-title">Mobile Phones</h5>
+                  <p class="card-text">Extra 30% off</p>
+                </div>
+              </div>
+              <div className="card d-none d-md-block">
+                <img
+                  src="https://s3-alpha-sig.figma.com/img/838c/fd20/c4c0e8f5b24776b743ae741f2ca7270e?Expires=1699833600&Signature=qE7gMMjn2zZZNQvQQNDquNdU52dEA9ABE~bkKjijYEwWMQUShIxcE21mOudeN1eOVxFZMW6DPuR6kBVUQhB0Nc6UgQO6bqQrQEdB06JWRj7~LWJouYpR0LSj9LEwEJGyBbq3mSF29Zi~9NEfODsBUpLjZL0ucaAtmbUiifp6ylzwS1Fadmpb2Hb72XnMM8vHFTYpa9Qrn-thEi9c6s5F-JDgv5BYWR4sy6IapMDSL0Ahgx-FSBctapi7~FIR5ORlj9mUDtCGz8X0mK0qwqm8Ey~p0Npk7Hh1I25s7zVHvferUxJUa~XXO5cBDfPhFa-NTFmlrBYo7UtDfLajLML2fw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div class="card-body">
+                  <h5 class="card-title">Watches</h5>
+                  <p class="card-text">Extra 30% off</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <div className="cards-wrapper">
+              <div className="card">
+                <img
+                  src="https://s3-alpha-sig.figma.com/img/0a93/0520/6a13d4463239359d29c9d81e2b24b42c?Expires=1699833600&Signature=lt6rd9IIdk22CSVOIf3kvoUtGEBgidJV9rPL1HbbCqzHMm~99QrqCM0vjk5NOUz-v8knsiZnolNCsV~YwxFO~5lNJb7BgChadNzzTgwUEdIECVuWOzYoy1Kbbq~Fls9pIgM3LNRaTVGIb5yo9QEJ9jE76iZ1GStbCSOvZOS5n15PPQmXwgHmyV6b7WAMfONvz0Jb9uqd6R5kfmEelC9-uy5dxrC1VxFHm7pM4wiLRvpTg2-o8VMuz5S1lyVjn54GKlD7PLzSTpt2YCMrKJ2~8vAnwZ4Sml~UmW-dvficAriElr5yyJPdfA7yF9DNl9S8hvq1hyaQojdJDFLeO-8m8g__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div class="card-body">
+                  <h5 class="card-title">Sun Glasses</h5>
+                  <p class="card-text">Extra 30% off</p>
+                </div>
+              </div>
+              <div className="card d-none d-md-block">
+                <img
+                  src="https://s3-alpha-sig.figma.com/img/50e5/9501/6317994781a1c2eda0bc69a932bd6f3f?Expires=1699833600&Signature=ECj5653myZGPrgm2xyd1JLQLAmtgYmrLAWJDXJxBT5Hsce2Z9HoAj8kYNYCAu4LygDndZOLwTG2OA~QV283kSU5aEhlEknU3D-AOLdtzwY7UMPjDphii5~YgnU8Gc9LOZ2Nj2AsXeaarv9WhrCt2A8dQEpq9OFqHnx29cVPVq0AI2bs1UwohcD5xy4yXiozzSSF-iCYobz45M9lybw2MmGAYWpLjI50EzakF5BwpK3cnkdXdCw7y~qiuxd~FJnkADdh7S4j~Fx94R2W05EFbsM6NOSEV4~0hay0i9eXO~YhMuz6h2ck~kcVsvpRWH6Rs~0cOTQGq9XTqiyMdamLOaQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div class="card-body">
+                  <h5 class="card-title">Men Shoes</h5>
+                  <p class="card-text">Extra 30% off</p>
+                </div>
+              </div>
+              <div className="card d-none d-md-block">
+                <img
+                  src="https://s3-alpha-sig.figma.com/img/8923/5459/e3b1cc3f6a9e3ad8bafb665e1eb54ab7?Expires=1699833600&Signature=eMEowDMnvUQR1I38DWphwfTuH9DWmV9jKqWPHtiBeu7BJJer2RLW7mYSIXzgR~pnRQuaufhlTIgdc6KlYEs2UL5RyPjbW2-SryC6N43V2xveXSCCRsjJRBeEFDr5VGTPBkha7YjmjDeVn4MLu3tjvU~UUsCOEdgjqH62j~EvYubqrLVuMYCraJ7Lz98QsNaOwLQEoMarl4bzcuws642hy55pFZs2AcYSWXDVMCHLv8ujrqPnusdv7XvoMc8mOwYdmsR1mwQS3cnbDqSiVSshTSWtypPph2wqExDIZ07j14Puf~59OO9caXjf1bF-pZL5tkQ7zVqut11Ub~ERAEI0gA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div class="card-body">
+                  <h5 class="card-title">Mobile Phones</h5>
+                  <p class="card-text">Extra 30% off</p>
+                </div>
+              </div>
+              <div className="card d-none d-md-block">
+                <img
+                  src="https://s3-alpha-sig.figma.com/img/838c/fd20/c4c0e8f5b24776b743ae741f2ca7270e?Expires=1699833600&Signature=qE7gMMjn2zZZNQvQQNDquNdU52dEA9ABE~bkKjijYEwWMQUShIxcE21mOudeN1eOVxFZMW6DPuR6kBVUQhB0Nc6UgQO6bqQrQEdB06JWRj7~LWJouYpR0LSj9LEwEJGyBbq3mSF29Zi~9NEfODsBUpLjZL0ucaAtmbUiifp6ylzwS1Fadmpb2Hb72XnMM8vHFTYpa9Qrn-thEi9c6s5F-JDgv5BYWR4sy6IapMDSL0Ahgx-FSBctapi7~FIR5ORlj9mUDtCGz8X0mK0qwqm8Ey~p0Npk7Hh1I25s7zVHvferUxJUa~XXO5cBDfPhFa-NTFmlrBYo7UtDfLajLML2fw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div class="card-body">
+                  <h5 class="card-title">Watches</h5>
+                  <p class="card-text">Extra 30% off</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="slideButtons">
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExample"
+          data-bs-slide="prev"
+        >
+          <span className="carousel-control-prev-icon" aria-hidden="true" />
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExample"
+          data-bs-slide="next"
+        >
+          <span className="carousel-control-next-icon" aria-hidden="true" />
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Trend;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
 import Banners from "../Components/Banners";
+import Trend from "../Components/Trend";
 
 import "./Home.css";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <Banners />
+      <div className="mbt">
+        <div className="title">
+          <p>New Trend</p>
+          <p>
+            <Link>View All</Link>
+          </p>
+        </div>
+        <Trend />
+      </div>
     </>
   );
 };
